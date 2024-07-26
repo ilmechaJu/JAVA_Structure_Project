c0258c1955a89346dc8d0937f97acf954304ca67
ilmechaJu <pawor006@naver.com>
2024-07-26 20:48:49 +0900

    Complete week4 Assignment

--- week04/Main.java
+++ week04/Main.java
@@ -4,5 +4,13 @@ package week04;
 public class Main {
     public static void main(String[] args){
         boolean calculateEnded = false;
+
+        while (!calculateEnded) {
+            try {
+                calculateEnded = CalculatorApp.start();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
---
+++ week04/Parser.java
@@ -0,0 +1,62 @@
+package week04;
+import java.util.regex.Pattern;
+
+public class Parser {
+    private static final String OPERATION_REG = "[+\\-*/]";
+    private static final String NUMBER_REG = "^[0-9]*$";
+
+    private final Calculator calculator = new Calculator();
+
+    public Parser parseFirstNum(String firstInput) throws Exception{
+
+        //정규식 체크
+        if (!Pattern.matches(NUMBER_REG, firstInput)){
+            throw new BadInputException("정수값");
+        }
+
+        // 구현 1.
+        this.calculator.setFirstNumber(Integer.parseInt(firstInput));
+
+        return this;
+    }
+
+    public Parser parseSecondNum(String secondInput) throws Exception{
+        //정규식 체크
+        if (!Pattern.matches(NUMBER_REG, secondInput)){
+            throw new BadInputException("정수값");
+        }
+
+        // 구현 1.
+        this.calculator.setSecondNumber(Integer.parseInt(secondInput));
+
+        return this;
+    }
+
+    public Parser parseOperator(String operationInput) throws Exception{
+
+        if (!Pattern.matches(OPERATION_REG, operationInput)){
+            throw new BadInputException("사칙 연산의 연산자");
+        }
+
+        switch (operationInput){
+            case "+":
+                this.calculator.setOperation(new AddOperation());
+                break;
+            case "-":
+                this.calculator.setOperation(new SubstractOperation());
+                break;
+            case "*":
+                this.calculator.setOperation(new MultiplyOperation());
+                break;
+            case "/":
+                this.calculator.setOperation(new DivideOperation());
+                break;
+        }
+        return this;
+    }
+
+    public double executeCalculator() {
+        return calculator.calculate();
+    }
+}
+
